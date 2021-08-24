661b9fe8e0d6ef78fd11f8c7fb9944f12c9e6287
udidolinski <udi672@gmail.com>
2021-08-24 19:58:20 +0300

    Initial document scanner

---
+++ scanning.py
@@ -0,0 +1,184 @@
+import numpy as np
+import cv2 as cv
+import imutils
+
+from perspective import four_point_transform
+
+from skimage.filters import threshold_local
+
+BOX_OFFSET = 0
+WHITE = 255
+BLACK = 0
+
+# Cosmetics
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+MEDIUM_LINE_LENGTH = 2
+
+# Hyper parameters
+EDGE_KERNEL_SIZE = 3
+PERI_PERCENTAGE = 0.001
+TRANSFORM_KERNEL_SIZE = 13
+THRESHOLD_KERNEL_SIZE = TRANSFORM_KERNEL_SIZE - 4
+THRESHOLD_BIAS = 2
+CANNY_T1 = 25
+CANNY_T2 = 320
+
+
+def find_edges(image_name):
+    '''
+    This function is for finding the edges in the image
+    :param image_name:
+    :return:
+    '''
+    image = cv.imread(f"input_images\\{image_name}.jpg")
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = imutils.resize(image, height=500)
+
+    # Changing the image to a BLACK & WHITE image
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    # Blurring the image to eliminate noises in the image
+    gray_image = cv.GaussianBlur(gray_image, (EDGE_KERNEL_SIZE, EDGE_KERNEL_SIZE), 0)
+
+    # med = np.median(gray_image)
+    # lower = int(max(0, 0.7* med))
+    # upper = int(min(255, 1.3*med))
+    # print(lower, upper)
+
+    # Using the Canny edge detection algorithm (with the gradient of the image),
+    # Intensity of an edge is the norm of (Gradient_x, Gradient_y).
+    # Any edge that has higher intensity than T2 is considered an edge.
+    # Any edge that has lower intensity than T2 (but higher than T1) and is connected to a considered edge, is also
+    # considered as an edge.
+    # Any other edge is not considered as an edge.
+    # L2gradient if TRUE,  using the norm 2 to calculate the intensity: sqrt of (Gradient_x**2 + Gradient_y**2).
+    #            if FALSE, using the norm 1 to calculate the intensity: |Gradient_x| + |Gradient_y**2|.
+    # apertureSize is the sobel kernel size (which is for smoothing the image in one direction and deriving the image
+    # on the perpendicular direction.
+    edged = cv.Canny(gray_image, CANNY_T1, CANNY_T2, L2gradient=True, apertureSize=3)
+    cv.imwrite(f"output_images\\{IMG_NAME}_edged.jpg", edged)
+    cv.imshow("Image", image)
+    cv.imshow("Gray Image", gray_image)
+    cv.imshow("Edged", edged)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return edged.copy(), image.copy(), orig, ratio
+
+
+def old_find_contour(edged, image):
+    cnts = cv.findContours(edged, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv.contourArea, reverse=True)[:5]
+    screenCnt = None
+
+    for c in cnts:
+
+        peri = cv.arcLength(c, True)
+        approx = cv.approxPolyDP(c, 0.02 * peri, True)
+
+        # finding an edge with 4 corners
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+
+    cv.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+    cv.imshow("Outline", image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def arc_length(contour):
+    '''
+    Calculating the length of a closed contour
+    :param contour:
+    :return:
+    '''
+    return cv.arcLength(contour, True)
+
+
+def find_contour(edged, image):
+    '''
+
+    :param edged: The edged image
+    :param image:
+    :return:
+    '''
+
+    # Finding all the contours in the edged image.
+    cnts = cv.findContours(edged, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # Sorting the closed contours by their lengths (maybe taking the max is enough).
+    cnts = sorted(cnts, key=arc_length, reverse=True)
+
+    # Calculating the closed contour length (which is the perimeter of the object).
+    peri = cv.arcLength(cnts[0], True)
+
+    # Approximating the the object contour (polynomial curve).
+    # The 2nd parameter is approximation accuracy, the closer it gets to 0 the tighter it fits the contour.
+    screen_contour = cv.approxPolyDP(cnts[0], PERI_PERCENTAGE * peri, True)
+
+    # Drawing the contour around the object
+    cv.drawContours(image, [screen_contour], -1, GREEN, MEDIUM_LINE_LENGTH)
+
+    # Finding the rotated rectangle that surrounds the object in order to know the angle for the the rotation.
+    rect = cv.minAreaRect(screen_contour)
+    box = cv.boxPoints(rect)
+    box = np.int0(box)
+
+    # Adding an offset to the box in order to catch a bit more than the object
+    box += np.array(
+        [[-BOX_OFFSET, -BOX_OFFSET], [BOX_OFFSET, -BOX_OFFSET], [BOX_OFFSET, BOX_OFFSET], [-BOX_OFFSET, BOX_OFFSET]])
+
+    # Drawing the rotated rectangle
+    cv.drawContours(image, [box], -1, RED, MEDIUM_LINE_LENGTH)
+    cv.imwrite(f"output_images\\{IMG_NAME}_boxed.jpg", image)
+    cv.imshow("Boxed", image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    return box
+
+
+def perspective_transform(org_image, screen_contour, ratio):
+    '''
+    This function is taking the object out of the image, rotating it and turning it to a clear white with black lines.
+    :param org_image: the original image
+    :param screen_contour: the contour of the object in the image
+    :param ratio: the ratio between the original and the processed image
+    :return: the final scanned image
+    '''
+    # Transforming the object from the image (which is rotating the object to be straight and discarding
+    # the rest of the image).
+    warped = four_point_transform(org_image, screen_contour.reshape(4, 2) * ratio)
+
+    # Changing the image to a BLACK & WHITE image
+    warped = cv.cvtColor(warped, cv.COLOR_BGR2GRAY)
+
+    # Blurring the image to eliminate noises in the image (maybe not needed).
+    warped = cv.GaussianBlur(warped, (TRANSFORM_KERNEL_SIZE, TRANSFORM_KERNEL_SIZE), 0)
+
+    # Thresholding the object using an adaptive gaussian threshold in order to eliminate noise and bold the data,
+    # changing the image to white with black lines.
+    warped = cv.adaptiveThreshold(warped, WHITE, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, THRESHOLD_KERNEL_SIZE,
+                                  THRESHOLD_BIAS)
+
+    # T = threshold_local(warped, 11, offset=10, method="gaussian")
+    # warped = (warped > T).astype(np.uint8) * 255
+
+    # ret3, warped = cv.threshold(warped, BLACK, WHITE, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    return warped
+
+
+if __name__ == '__main__':
+    IMG_NAME = "receipt"
+    edged_image, resized_image, org_image, ratio = find_edges(IMG_NAME)
+    screen_contour = find_contour(edged_image, resized_image)
+    scanned_image = perspective_transform(org_image, screen_contour, ratio)
+    cv.imwrite(f"output_images\\{IMG_NAME}_scanned.jpg", scanned_image)
+    cv.imshow("Original", imutils.resize(org_image, height=650))
+    cv.imshow("Scanned", imutils.resize(scanned_image, height=650))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
