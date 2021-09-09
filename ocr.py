726717b4c721c8a7e75ff95f0a63d2fda69b5cd7
udidolinski <udi672@gmail.com>
2021-09-09 18:06:20 +0300

    added erosion and dilation tests and rescaling the image

--- ocr.py
+++ ocr.py
@@ -1,8 +1,10 @@
+import numpy as np
 import pytesseract
 import cv2 as cv
 import re
 import os
 
+import skimage.transform
 from multilingual_pdf2text.pdf2text import PDF2Text
 from multilingual_pdf2text.models.document_model.document import Document
 from scanning import preprocess_image, thresholding
@@ -53,6 +55,88 @@ def text_outputs(image_name):
         out2.write(c2)
 
 
+def erosion_test(file_name):
+    """
+    This function is thickening the text.
+    :param file_name:
+    :return:
+    """
+    image = cv.imread(f"input_images/{file_name}")
+    kernel_size = 3
+    clean_image = thresholding(image)
+    kernel2 = np.ones((kernel_size, kernel_size), np.uint8)
+    kernel2[0, 0] = 0
+    kernel2[0, 2] = 0
+    kernel2[2, 0] = 0
+    kernel2[2, 2] = 0
+    eroded = cv.erode(clean_image, kernel2)
+    eroded_original = cv.erode(image, kernel2)
+    cv.imshow("clean image", clean_image)
+    cv.imshow("Eroded", eroded)
+    cv.imshow("original image", image)
+    cv.imshow("eroded original", eroded_original)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    text1 = pytesseract.image_to_string(image, lang='heb')
+    text2 = pytesseract.image_to_string(clean_image, lang='heb')
+    text3 = pytesseract.image_to_string(eroded, lang='heb')
+    text4 = pytesseract.image_to_string(eroded_original, lang='heb')
+
+    with open("file1.txt", "w") as f1:
+        f1.write(text1)
+    with open("file2.txt", "w") as f1:
+        f1.write(text2)
+    with open("file3.txt", "w") as f1:
+        f1.write(text3)
+    with open("file4.txt", "w") as f1:
+        f1.write(text4)
+
+
+def dilation_test(file_name):
+    """
+    This function is thinning the text in the image.
+    :param file_name:
+    :return:
+    """
+    rescale_factor = 1.5
+    image = cv.imread(f"input_images/{file_name}")
+    # Enlarging the image by factor of 1.5
+    image = np.uint8(skimage.transform.rescale(image, (rescale_factor, rescale_factor), multichannel=True, preserve_range=True))
+    kernel_size = 3
+    clean_image = thresholding(image)
+    kernel2 = np.ones((kernel_size, kernel_size), np.uint8)
+    kernel2[0, 0] = 0
+    kernel2[0, 2] = 0
+    kernel2[2, 0] = 0
+    kernel2[2, 2] = 0
+
+    dilated = cv.dilate(clean_image, kernel2)
+
+    original_dilated = cv.dilate(image, kernel2)
+
+    # test_txts(image, clean_image, dilated, original_dilated)
+    cv.imshow("original image", image)
+    cv.imshow("clean image", clean_image)
+    cv.imshow("dilated", dilated)
+    cv.imshow("dilated original", original_dilated)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def test_txts(*images):
+    """
+    This function extracts the text from any amount of images and writes it into text files.
+    :param images:
+    :return:
+    """
+    for i, image in enumerate(images):
+        text = pytesseract.image_to_string(image, lang='heb')
+        with open(f"test_file{i + 1}.txt", "w") as f:
+            f.write(text)
+
+
 if __name__ == '__main__':
-    for image_name in os.listdir("input_images"):
-        text_outputs(image_name)
+    # for image_name in os.listdir("input_images"):
+    #     text_outputs(image_name)
+    dilation_test("divorce_1.jpg")
